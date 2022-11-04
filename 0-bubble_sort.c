09bcd47f38751c325ef7e79e54bd5d1c13f1d574
adegenius7 <adegenius007@gmail.com>
2022-11-04 23:47:39 +0000

    header files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * bubble_sort- sort an array of integers
+ *
+ * @array- array pointer
+ *
+ */
+void bubble_sort(int *array, size_t size);
+{
+	int temp, j
